4a89a4ecf2f8e92171fa79a1f7e8a28dbeb62400
Nathan Carter <ncarter@bentley.edu>
2019-03-21 17:37:34 +0000

    Source docs now in correct fmt, plus doc build script

--- lib/main.gd
+++ lib/main.gd
@@ -1,121 +1,208 @@
+############################################################################
+##
+##
+#W  main.gd              SemigroupViz Package                Nathan Carter
+##
+##  Declaration file for functions of the SemigroupViz package.
+##
+#Y  Copyright (C) 2019 University of St. Andrews, North Haugh,
+#Y                     St. Andrews, Fife KY16 9SS, Scotland
+##
 
+#! @Chapter Function reference
+#! @ChapterLabel funcref
 
-# This function converts the given H-class from the given semigroup
-# into a GAP record, obeying the options passed in the third argument.
-#
-# All records formed by this package are of a form amenable to
-# conversion to JSON via GAP's JSON package.
-#
-# The first parameter must be a semigroup, as created by GAP's
-# Semigroups Package.
-# The second argument must be a single H-class from that semigroup,
-# which you can form in a number of ways, such as by choosing a
-# D-class, lifting out its first R- and L-classes, and interesecting
-# them.
-# The final argument should be a GAP record whose contents will
-# govern how this function does its work, as indicated below.
-#
-# Attributes in the resulting record include:
-#  - size := the number of elements in the H-class
-#  - representative := a string representation of the H-class's
-#        representative.  The string representation will be computed
-#        by passing the H-class's representative element to the
-#        options.ToString function.
-#  - elements := an array of string representations of all the
-#        elements in the class, by default, or some subset of them
-#        if options.NrElementsIncludedPerHClass > 0 and smaller than
-#        the number of elements in this particular class.  The
-#        representative is always first on this list.  These string
-#        representations are also computed by options.ToString.
+#! @Section Private API
+
+#! @Arguments semigroup, hclass, options
+#! @Returns a &GAP; record obeying the options passed in the third argument
+#! @Description
+#!  This function converts the given H-class from the given semigroup
+#!  into a &GAP; record amenable to conversion to JSON for passing to one
+#!  of the HTML-based visualization tools from the
+#!  <Package>JupyterViz</Package> package.
+#!  Such conversion can be done by &GAP;'s <Package>JSON</Package> package.
+#!
+#!  <List>
+#!    <Item>The first parameter must be a semigroup, as created by &GAP;'s
+#!      <Package>Semigroups</Package> Package.</Item>
+#!    <Item>The second argument must be a single H-class from that semigroup,
+#!      which you can form in a number of ways, such as by choosing a
+#!      D-class, lifting out its first R- and L-classes, and interesecting
+#!      them.</Item>
+#!    <Item>The final argument should be a &GAP; record whose contents will
+#!      govern how this function does its work, as indicated below.</Item>
+#!  </List>
+#!
+#!  Attributes in the resulting record include:
+#!  <List>
+#!    <Item><Code>size</Code> is the number of elements in the H-class</Item>
+#!    <Item><Code>representative</Code> is a string representation of the
+#!      H-class's representative.  The string representation will be computed
+#!      by passing the H-class's representative element to the
+#!      <Code>options.ToString</Code> function.</Item>
+#!    <Item><Code>elements</Code> is an array of string representations of
+#!      all the elements in the class, by default, or some subset of them
+#!      if <Code>options.NrElementsIncludedPerHClass > 0</Code> and smaller
+#!      than the number of elements in this particular class.  The
+#!      representative is always first on this list.  These string
+#!      representations are also computed by <Code>options.ToString</Code>.
+#!       </Item>
+#!  </List>
 DeclareGlobalFunction( "HClassToRecord" );
 
 
-# This function converts the given D-class from the given semigroup
-# into a GAP record, obeying the options passed in the third argument.
-#
-# All records formed by this package are of a form amenable to
-# conversion to JSON via GAP's JSON package.
-#
-# The first parameter must be a semigroup, as created by GAP's
-# Semigroups Package.
-# The second argument must be a single D-class from that semigroup,
-# which you can form in a number of ways, including
-# DClasses(semigroup)[1].
-# The final argument should be a GAP record whose contents will
-# govern how this function does its work, as indicated below.
-#
-# Attributes in the resulting record include:
-#  - size := the number of R-classes in the D-class
-#  - RClasses := an array of records, each of which has the
-#        following attributes:
-#    - size := the number of L-classes in the R-class (though not
-#        all of these may be included in the return value; you can
-#        set a smaller limit in options.NrRClassesIncludedPerDClass)
-#    - HClasses := an array of records, each of which is produced
-#        by a call to HClassToRecord(), passing this semigroup,
-#        one of its H-classes, and the same options object passed
-#        to us.  See that function, above, for details on its
-#        output format.  (Not all H-classes may be included in the
-#        return value; you can set a smaller limit in
-#        options.NrLClassesIncludedPerRClass, recalling that a
-#        choice of L-class and R-class determines an H-class.)
+#! @Arguments semigroup, dclass, options
+#! @Returns a &GAP; record obeying the options passed in the third argument
+#! @Description
+#!  This function converts the given D-class from the given semigroup
+#!  into a &GAP; record amenable to conversion to JSON for passing to one
+#!  of the HTML-based visualization tools from the
+#!  <Package>JupyterViz</Package> package.
+#!  Such conversion can be done by &GAP;'s <Package>JSON</Package> package.
+#!
+#!  <List>
+#!    <Item>The first parameter must be a semigroup, as created by &GAP;'s
+#!      <Package>Semigroups</Package> Package.</Item>
+#!    <Item>The second argument must be a single D-class from that semigroup,
+#!      which you can form in a number of ways, such as
+#!      <Code>Dclasses(semigroup)[1]</Code>.</Item>
+#!    <Item>The final argument should be a &GAP; record whose contents will
+#!      govern how this function does its work, as indicated below.</Item>
+#!  </List>
+#!
+#!  Attributes in the resulting record include:
+#!  <List>
+#!    <Item><Code>size</Code> is the number of R-classes in the D-class</Item>
+#!    <Item><Code>RClasses</Code> is an array of records,
+#!        each of which has the following attributes:
+#!      <List>
+#!        <Item><Code>size</Code> is the number of L-classes in the R-class
+#!          (though not all of these need be included in the return value;
+#!          you can set a smaller limit in
+#!          <Code>options.NrRClassesIncludedPerDClass</Code>)</Item>
+#!        <Item><Code>HClasses</Code> is an array of records,
+#!          each of which is produced by a call to
+#!          <Ref Func="HClassToRecord"/>, passing this
+#!          <Code>semigroup</Code>, one of its H-classes, and the same
+#!          <Code>options</Code> object passed to us.  See that function,
+#!          above, for details on its output format.
+#!          (Not all H-classes need be included in the return value;
+#!          you can set a smaller limit in
+#!          <Code>options.NrLClassesIncludedPerRClass</Code>, recalling that
+#!          choosing an L-class and an R-class determines an H-class.)</Item>
+#!      </List>
+#!    </Item>
+#!  </List>
 DeclareGlobalFunction( "DClassToRecord" );
 
 
-# This function converts the given semigroup into a GAP record,
-# obeying the options passed in the second argument.
-#
-# All records formed by this package are of a form amenable to
-# conversion to JSON via GAP's JSON package.
-#
-# The first parameter must be a semigroup, as created by GAP's
-# Semigroups Package.
-# The second argument should be a GAP record whose contents will
-# govern how this function does its work, as indicated below.
-#
-# Attributes in the resulting record include:
-#  - name := a string describing the semigroup.  This will come
-#        directly from options.name, if you include such a field,
-#        or will be computed from ViewString(semigroup) if you do
-#        not, removing the <, >, \<, and \> characters first.
-#  - size := the number of D-classes in the semigroup
-#  - DClasses := an array of records, each of which has the
-#        following attributes:
-#    - size := the number of L-classes in the R-class (though not
-#        all of these may be included in the return value; you can
-#        set a smaller limit in options.NrRClassesIncludedPerDClass)
-#    - DClasses := an array of records, each of which is produced
-#        by a call to DClassToRecord(), passing this semigroup,
-#        one of its D-classes, and the same options object passed
-#        to us.  See that function, above, for details on its
-#        output format.  (Not all D-classes may be included in the
-#        return value; you can set a smaller limit in
-#        options.NrDClassesIncluded.)
-#  - options := a copy of the options record passed as input,
-#        with the exception of the ToString and name fields
+#! @Arguments semigroup, options
+#! @Returns a &GAP; record obeying the options passed in the third argument
+#! @Description
+#!  This function converts the given semigroup into a &GAP; record
+#!  amenable to conversion to JSON for passing to one of the HTML-based
+#!  visualization tools from the <Package>JupyterViz</Package> package.
+#!  Such conversion can be done by &GAP;'s <Package>JSON</Package> package.
+#!
+#!  <List>
+#!    <Item>The first parameter must be a semigroup, as created by &GAP;'s
+#!      <Package>Semigroups</Package> Package.</Item>
+#!    <Item>The second argument should be a &GAP; record whose contents will
+#!      govern how this function does its work, as indicated below.</Item>
+#!  </List>
+#!
+#!  Attributes in the resulting record include:
+#!  <List>
+#!    <Item><Code>name</Code> is a string describing the semigroup.
+#!      This will come directly from <Code>options.name</Code> if you
+#!      include such a field, or will be computed from
+#!      <Code>ViewString(semigroup)</Code> if you do not (by removing
+#!      angle brackets and line break characters).</Item>
+#!    <Item><Code>size</Code> is the number of D-classes in the
+#!      semigroup</Item>
+#!    <Item><Code>DClasses</Code> is an array of records,
+#!      each of which is produced by a call to
+#!      <Ref Func="DClassToRecord"/>, passing this
+#!      <Code>semigroup</Code>, one of its D-classes, and the same
+#!      <Code>options</Code> object passed to us.  See that function,
+#!      above, for details on its output format.
+#!      (Not all D-classes need be included in the return value;
+#!      you can set a smaller limit in
+#!      <Code>options.NrDClassesIncluded</Code>.)</Item>
+#!    <Item><Code>options</Code> is a copy of the options record
+#!      passed as the second parameter, with the exception of its
+#!      <Code>ToString</Code> and <Code>name</Code> fields.  (This is
+#!      because the <Code>ToString</Code> field is a function, and
+#!      thus not amenable to JSON conversion, and the <Code>name</Code>
+#!      field has already been included as the <Code>name</Code> field
+#!      of the entire return value.)</Item>
+#!  </List>
 DeclareGlobalFunction( "EggBoxDiagramRecord" );
 
 
-# Documented options:
-# ToString: a function mapping elements of the semigroup to strings.
-#   This defaults to PrintString, but if you have something better,
-#   go ahead and provide it.
-# NrDClassesIncluded: maximum number of D-classes to compute and
-#   include in the JSON object to be visualized.
-#   This is useful if the semigroup is extremely large, and you want
-#   to send a manageable amount of JSON data to the visualization,
-#   to see a subset of the semigroup.
-#   (defaults to 20, set to 0 for no limit)
-# NrRClassesIncludedPerDClass: maximum number of R-classes to
-#   compute for any one D-class and include in the JSON.
-#   (defaults to 20, set to 0 for no limit)
-#   See above for explanation of why you might want to do this.
-# NrLClassesIncludedPerRClass: maximum number of L-classes to
-#   compute for any one D/R-class and include in the JSON.
-#   (defaults to 20, set to 0 for no limit)
-#   See above for explanation of why you might want to do this.
-# NrElementsIncludedPerHClass: maximum number of elements to
-#   compute for any one H-class and include in the JSON.
-#   (defaults to 20, set to 0 for no limit)
-#   See above for explanation of why you might want to do this.
+#! @Section Public API
+
+#! @Arguments semigroup[, options]
+#! @Returns nothing
+#! @Description
+#!  This function displays a visualization of the given semigroup to the
+#!  user by calling the display tools in the underlying
+#!  <Package>JupyterViz</Package> package.  This means that one of two
+#!  methods will be used for showing the user the resulting visualization:
+#!  <List>
+#!    <Item>If this function is called in a Jupyter Notebook, it returns
+#!      an object that, when rendered by that notebook, will result in
+#!      the visualization appearing in the correct output cell.</Item>
+#!    <Item>If run outside of a Jupyter Notebook, such as in the &GAP;
+#!      REPL, this function creates an HTML page containing the given
+#!      visualization and then opens the page in the system default web
+#!      browser.</Item>
+#!  </List>
+#!
+#!  It accepts the following arguments.
+#!  <List>
+#!    <Item>The first parameter must be a semigroup, as created by &GAP;'s
+#!      <Package>Semigroups</Package> Package.</Item>
+#!    <Item>The second argument is optional but if it is present, it
+#!      should be a &GAP; record whose contents will
+#!      govern how this function does its work, as indicated below.</Item>
+#!  </List>
+#!
+#!  The fields you can provide in the <Code>options</Code> parameter are
+#!  as follows.  Each is optional, and its default value is documented
+#!  below.
+#!  <List>
+#!    <Item><Code>ToString</Code> can be a function mapping elements of
+#!      the semigroup to strings, as documented in
+#!      <Ref Func="HClassToRecord"/>.</Item>
+#!    <Item><Code>NrDClassesIncluded</Code> can be the maximum number of
+#!      D-classes to compute and include in the JSON object to be
+#!      visualized.  This is useful if the semigroup is extremely large,
+#!      and you want to send only a manageable amount of JSON data to
+#!      be displayed, so that the computation becomes feasible.
+#!      This defaults to 20.  Set it to 0 for no limit (if you know your
+#!      semigroup is of a reasonable size).</Item>
+#!    <Item><Code>NrRClassesIncludedPerDClass</Code> can be the maximum
+#!      number of R-classes to compute for any one D-class and include
+#!      in the JSON, for the same reasons as described above for
+#!      <Code>NrDClassesIncluded</Code>.
+#!      This defaults to 20.  Set it to 0 for no limit (if you know your
+#!      semigroup's D-classes have a reasonable number of R-classes
+#!      each).</Item>
+#!    <Item><Code>NrLClassesIncludedPerRClass</Code> can be the maximum
+#!      number of L-classes to compute for any one D/R-class and include
+#!      in the JSON, for the same reasons as described above for
+#!      <Code>NrDClassesIncluded</Code>.
+#!      This defaults to 20.  Set it to 0 for no limit (if you know your
+#!      semigroup's D-classes have a reasonable number of L-classes
+#!      each).</Item>
+#!    <Item><Code>NrElementsIncludedPerHClass</Code> can be the maximum
+#!      number of elements to compute for any one H-class and include
+#!      in the JSON, for the same reasons as described above for
+#!      <Code>NrDClassesIncluded</Code>.
+#!      This defaults to 20.  Set it to 0 for no limit (if you know your
+#!      semigroup's H-classes have a reasonable number of elements
+#!      each).</Item>
+#!  </List>
 DeclareGlobalFunction( "ShowSemigroup" );
--- to-do-list.md
+++ to-do-list.md
@@ -8,9 +8,7 @@
 
 ## Packaging
 
- * Update the documentation in the `.gd` file to use GAPDoc's format.
  * Create the other files necessary to create a manual.
- * Make a build script for the manual.
  * Write code for testing this package together with a master test script.
  * Publish this to GitHub.
  * If Steve's changes have been included, ask Alex if we can include this
